25a8b669cb60ec59ae1dbdd99977d904a110ed3e
webmaster-alcartagena <webmaster@cartagena.gov.co>
2023-03-06 21:22:31 -0500

    firt commit

---
+++ src/main/java/co/edu/unicolombo/ingsistemas/ApiRestElectiva/ApiRestElectivaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicolombo.ingsistemas.ApiRestElectiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestElectivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestElectivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unicolombo/ingsistemas/ApiRestElectiva/ApiRestElectivaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicolombo.ingsistemas.ApiRestElectiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestElectivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
